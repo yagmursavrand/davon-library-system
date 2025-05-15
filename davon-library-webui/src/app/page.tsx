661961df2d36080c143ecde1afb57b7fce79bd46
canan savran <canansavran@canan-MacBook-Air.local>
2025-05-15 14:42:03 +0300

    Added davon-library-webui with css modules

---
+++ davon-library-webui/src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Davon Library System</h1>
+      <p>Welcome to our library management system</p>
+    </main>
+  );
+} 
